f146d2f8cf3b09ee6ebd7b07b9dba102e2986745
yufeibabybear <84059047+yufeibabybear@users.noreply.github.com>
2021-05-23 22:36:31 +0800

    add Game_0519

---
+++ Game_0519/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+
+    public int difficulty;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        button.onClick.AddListener(setDifficulty);      
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void setDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+    }
+}
